a36d690a2726ac8606619a65d7599d010de5c94d
dmychel <dmychel@tutanota.com>
2024-03-16 10:29:13 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import '/src/index.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div
+        className='sprite'>
+        <span>
+          sprite
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default App
