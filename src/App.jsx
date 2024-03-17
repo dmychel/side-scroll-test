3eb03a9dd2fa94f6bced43a8041cdcdfe196ea39
dmychel <dmychel@tutanota.com>
2024-03-17 08:56:45 -0400

    added jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,49 @@ import '/src/index.css'
 function App() {
   return (
     <div className="app">
+
+      <div className="background">
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+        <img src="/assets/tree.png" alt="" />
+      </div>
+
       <div
         className='sprite'>
         <span>
           sprite
         </span>
       </div>
+
     </div>
   )
 }
