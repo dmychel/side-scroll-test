c1cf38eafda28218217cf11791c145d339ce0f6b
dmychel <dmychel@tutanota.com>
2024-03-20 15:05:45 -0400

    jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
 import { useEffect, useState } from "react";
 import "/src/index.css";
-import { ssrExportAllKey } from "vite/runtime";
 
 function App() {
-  const elementArr = ['yellow', 'orange', 'red']
-  const [element, setElement] = useState('orange')
-
-
+  const elementArr = ["yellow", "orange", "red"];
+  const [element, setElement] = useState("orange");
 
   useEffect(() => {
-    document.getElementById(element).scrollIntoView({ behavior: 'smooth' })
-  }, [element])
+    document.getElementById(element).scrollIntoView({ behavior: "smooth" });
+  }, [element]);
 
   function scrollTo(direction) {
-    const index = elementArr.indexOf(element)
+    const index = elementArr.indexOf(element);
     if (direction === true) {
-      const newEl = elementArr[index + 1]
-      setElement(newEl)
-      console.log(newEl)
+      const newEl = elementArr[index + 1];
+      setElement(newEl);
+      console.log(newEl);
+    } else {
+      const newEl = elementArr[index - 1];
+      setElement(newEl);
+      console.log(newEl);
     }
-    else {
-      const newEl = elementArr[index - 1]
-      setElement(newEl)
-      console.log(newEl)
-    }
-    // console.log(index)
-    console.log(direction)
-    // console.log(element)
   }
 
   return (
     <div className="app">
-      <div className="sprite-container">
-        <span className="sprite"></span>
-        <nav>
-          <button onClick={() => scrollTo(false)}>scroll Left</button>
-          <button onClick={() => scrollTo(true)}>scroll right</button>
-        </nav>
+      <nav className="nav_container">
+        <button onClick={() => scrollTo(false)}>scroll Left</button>
+        <button onClick={() => scrollTo(true)}>scroll right</button>
+      </nav>
+
+      <div className="sprite_container">
+        <img src="/assets/karatecat.png" className="sprite" />
+        <span className="path"></span>
       </div>
+
       <section className="yellow" id="yellow">
         <h1>Left</h1>
       </section>
