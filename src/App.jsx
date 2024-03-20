40a9b7b599d487a282c52cd1421badb5900ec400
dmychel <dmychel@tutanota.com>
2024-03-20 09:15:57 -0400

    changed jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,16 @@ import '/src/index.css'
 function App() {
   return (
     <div className="app">
-
-      <div className="background">
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-        <img src="/assets/tree.png" alt="" />
-      </div>
-
       <div
         className='sprite'>
         <span>
-          sprite
         </span>
       </div>
+      <section className='yellow' id='yellow'><h1>Left</h1></section>
 
+      <section className='orange' id='orane'><h1>Middle</h1></section>
+
+      <section className='red' id='red'><h1>Red</h1></section>
     </div>
   )
 }
