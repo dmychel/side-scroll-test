6baef420c2b88a7e4d0bd38a702ac3211fa54ed2
dmychel <dmychel@tutanota.com>
2024-03-20 10:04:35 -0400

    Navigation Logic
    The logic is essentially a manual image carousel.
    
    Created a useEffect statement that listens for the useState 'element'. When the element useState is updated the useEffect scrolls into view the value of element.
    
    The jsx has two buttons: scroll left and scroll right. Each button has an onclick evenetlistener that calls the function 'scrollTo.
    
    The scrollTo function takes an arguement of true or false. The function creates a variable to be equal to the index of the elementsArr based on the 'elements' useState.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
-import '/src/index.css'
+import { useEffect, useState } from "react";
+import "/src/index.css";
+import { ssrExportAllKey } from "vite/runtime";
 
 function App() {
+  const elementArr = ['yellow', 'orange', 'red']
+  const [element, setElement] = useState('orange')
+
+
+
+  useEffect(() => {
+    document.getElementById(element).scrollIntoView({ behavior: 'smooth' })
+  }, [element])
+
+  function scrollTo(direction) {
+    const index = elementArr.indexOf(element)
+    if (direction === true) {
+      const newEl = elementArr[index + 1]
+      setElement(newEl)
+      console.log(newEl)
+    }
+    else {
+      const newEl = elementArr[index - 1]
+      setElement(newEl)
+      console.log(newEl)
+    }
+    // console.log(index)
+    console.log(direction)
+    // console.log(element)
+  }
+
   return (
     <div className="app">
-      <div
-        className='sprite'>
-        <span>
-        </span>
+      <div className="sprite-container">
+        <span className="sprite"></span>
+        <nav>
+          <button onClick={() => scrollTo(false)}>scroll Left</button>
+          <button onClick={() => scrollTo(true)}>scroll right</button>
+        </nav>
       </div>
-      <section className='yellow' id='yellow'><h1>Left</h1></section>
+      <section className="yellow" id="yellow">
+        <h1>Left</h1>
+      </section>
 
-      <section className='orange' id='orane'><h1>Middle</h1></section>
+      <section className="orange" id="orange">
+        <h1>Middle</h1>
+      </section>
 
-      <section className='red' id='red'><h1>Red</h1></section>
+      <section className="red" id="red">
+        <h1>Right</h1>
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
